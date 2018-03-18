567fc5d177306064276d869b92109e696e0b4835
Maduma Nzungani <maduma@pt.lu>
2018-03-18 17:06:40 +0000

    add router and key_pair

--- trystack.tf
+++ trystack.tf
@@ -6,6 +6,11 @@ provider "openstack" {
   region      = "RegionOne"
 }
 
+resource "openstack_compute_keypair_v2" "snsakala" {
+  name       = "snsakala"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAIBlF377o8DpYop4x9kDhFJ4lpHnzDpOUtKtI4QI5OTmvWX3cld0ga2N2opQUCGJuUbRWaVyhLKOBobJ5yfUc3wP1l9F5IAylDH2EDRKGiohA2vbK37k7Tv6kiQs/IfZ2DL11uYqHjfc72efak88NYzYi3Z3Jj+65EfnDFmTMB+Yow== Stephane Nsakala"
+}
+
 resource "openstack_networking_router_v2" "router" {
   name = "router"
 }
