a40fe2a568c69ffc5a550d2a1c06e33a580134a0
Maduma Nzungani <maduma@pt.lu>
2018-03-18 17:14:44 +0000

    add admin security group - ssh and ping

--- trystack.tf
+++ trystack.tf
@@ -11,6 +11,28 @@ resource "openstack_compute_keypair_v2" "snsakala" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAIBlF377o8DpYop4x9kDhFJ4lpHnzDpOUtKtI4QI5OTmvWX3cld0ga2N2opQUCGJuUbRWaVyhLKOBobJ5yfUc3wP1l9F5IAylDH2EDRKGiohA2vbK37k7Tv6kiQs/IfZ2DL11uYqHjfc72efak88NYzYi3Z3Jj+65EfnDFmTMB+Yow== Stephane Nsakala"
 }
 
+resource "openstack_networking_secgroup_v2" "admin" {
+  name        = "admin"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "ssh" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.admin.id}"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "ping" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "icmp"
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.admin.id}"
+}
+
 resource "openstack_networking_router_v2" "router" {
   name = "router"
 }
